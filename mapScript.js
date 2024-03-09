f41bb4a43a24a9e7de8c8e0d3abdb54e2881532b
Rael Watt <55397739+rnwatt@users.noreply.github.com>
2024-03-09 16:54:31 +0000

    Update mapScript.js
    uk map and points, need data

--- mapScript.js
+++ mapScript.js
@@ -1,29 +1,82 @@
-//Sets options for the map on creation
-var mapOptions = {
-    center: [54.78, -4],
-    zoom: 5.5
-}
+anychart.onDocumentReady(function () {
+      // the data for this sample is stored at
+      // https://cdn.anychart.com/samples/maps-connectors/trip-through-the-uk/data.js
+      // Creates map chart
+      var map = anychart.connector();
 
-//creates map
-var map = new L.map("map", mapOptions);
-var marker = undefined;
-var layer = new
-L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-});
-map.addLayer(layer);
-var markersArray = [];
-var radius = 10;
+      map.unboundRegions().enabled(true).fill('#E1E1E1').stroke('#D2D2D2');
 
-function createNode(element) {
-    return document.createElement(element);
-}
+      // Sets settings for map chart
+      map.geoData('anychart.maps.united_kingdom');
+      map.interactivity().selectionMode('none');
 
-function append(parent, el) {
-    return parent.appendChild(el);
-}
+      // Sets title for map chart and customizes it
+      map
+        .title()
+        .enabled(true)
+        .useHtml(true)
 
-function changeRadius(){
-    rangeElement = document.getElementById("radius");
-    radius = rangeElement.value;
-}
+      var citiesSeries = map
+        .marker(getCitiesData()) 
+        .type('circle')
+        .size(4)
+        .fill('#64b5f6')
+        .stroke('2 #E1E1E1')
+        .tooltip(false);
+
+      citiesSeries.hovered().size(4).fill('#64b5f6').stroke('2 #E1E1E1');
+
+      // Customizes labels for the airport names series
+      citiesSeries
+        .labels()
+        .enabled(true)
+        .position('center-bottom')
+        .fontColor('#263238')
+        .offsetY(0)
+        .offsetX(5)
+        .anchor('left-center')
+        .format('{%name}');
+
+      // exclude cities from the legend
+      citiesSeries.legendItem(false);
+
+      // create zoom controls
+      var zoomController = anychart.ui.zoom();
+      zoomController.render(map);
+
+      // Sets container id for the chart
+      map.container('container');
+
+      // Initiates chart drawing
+      map.draw();
+    });
+
+    // Helper function to create several series
+    function createSeries(map, name, data, color) {
+      // Creates connector series and customizes them
+      var connectorSeries = map
+        .connector(data)
+        .startSize(0)
+        .endSize(0)
+        .stroke('2 ' + color)
+        .name(name)
+        .curvature(0);
+      connectorSeries.legendItem({
+        iconType: 'circle',
+        fill: color,
+        iconStroke: '2 #E1E1E1'
+      });
+      connectorSeries.hovered().stroke('1.5 #212121');
+
+      // Customizes tooltips for the destination series
+      connectorSeries
+        .tooltip()
+        .padding([8, 13, 10, 13])
+        .fontSize(12)
+        .fontColor('#fff')
+        .format('{%full}')
+        .titleFormat(
+          '{%number}. <span style="font-size: 13px; color: #E1E1E1">{%short}</span>'
+        )
+        .title({ useHtml: true });
+    }
