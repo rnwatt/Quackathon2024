388d8ae42d05ced3d5c0bb5c0cfd9d3d8f152f65
Kayee Liu <kasterune@gmail.com>
2024-03-09 15:34:59 +0000

    adding base route

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://kasterune:awUytJIxf9r0yrCB@quackathondataset.8crlzqr.mongodb.net/?retryWrites=true&w=majority&appName=QuackathonDataset";
 const app = express();
 const PORT = 3333;
+app.use(express.urlencoded());
+
+// CORS 
+app.use(cors({
+  origin: '*'
+}));
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -15,9 +21,41 @@ const client = new MongoClient(uri, {
   }
 });
 
+let dbConnection;
+module.exports = {
+  connectToServer: function(callback) {
+    client.connect (function (err, db) {
+      if (err || !db) {
+        return callback(err);
+      }
+
+      dbConnection = db.db("quackathonDataset");
+      console.log("Successful connection to MongoDB");
+
+      return callback();
+    });
+  },
+
+  getDb: function () {
+    return dbConnection;
+  },
+};
+
 // Defining routes
+const routes = express.Router();
+routes.route("/atm").get(async function (req, res) {
+  const dbConnect = dbo.getDb();
 
-
+  dbConnect.collection("quackathonCollection")
+  .find({}).limit(50)
+  .toArray(function (err, result) {
+    if (err) {
+      res.status(400).send("Error fetching.");
+    } else {
+      res.json(result);
+    }
+  });
+});
 
 // Listening to server
 app.listen(PORT, () => {
